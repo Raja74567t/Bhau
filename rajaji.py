b43a4be48cf72084bc0796ff27a0cdf38b256eba
Raja74567t <rambabusahu122@gmail.com>
2024-12-13 20:54:07 +0530

    Update rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -2,7 +2,7 @@
 #SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7841675622:AAHwFv4tOtjFGxCh-dfVzQdm-u3wX_6vPmc")
+BOT_TOKEN=("7841675622:AAFPf4Qj4j6Elii1GoPzXEhY9zdA2hK8NDE")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("rajaraj_04")
